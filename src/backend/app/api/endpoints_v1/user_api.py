aaa60edff3589ccec8121f642372e275a5ac2c7b
Ari Efendi <ariefendi992@gmail.com>
2023-12-11 12:18:49 +0800

    add new endpoint

--- src/backend/app/api/endpoints_v1/user_api.py
+++ src/backend/app/api/endpoints_v1/user_api.py
@@ -1,15 +1,41 @@
 from typing import List
-from sqlalchemy import select
-from fastapi import APIRouter
-from app.core.dependencies import SessionDepends
+from sqlalchemy import Select, select
+from fastapi import APIRouter, HTTPException
+from app.core.dependencies import SessionDepends, gen_password_hash
 from app.models.user_model import UserModel
-from app.schemas.user_schema import UserOutSchm
+from app.schemas.user_schema import UserCreateSchm, UserInDBSchm, UserOutSchm
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[UserOutSchm])
-async def get(*, db: SessionDepends, skip: int = 0, limit: int = 100):
+@router.get("/", response_model=List[UserOutSchm], summary="All Users")
+async def read_users(*, db: SessionDepends, skip: int = 0, limit: int = 100):
     db_stmt = await db.execute(select(UserModel).offset(skip).limit(limit))
     db_result = db_stmt.scalars()
     return db_result
+
+
+@router.post("/", response_model=UserOutSchm, summary="Create User")
+async def create_user(*, db: SessionDepends, user: UserCreateSchm):
+    db_stmt = await db.execute(
+        Select(UserModel).filter(UserModel.username == user.username)
+    )
+    result_user = db_stmt.scalar()
+
+    if result_user:
+        raise HTTPException(
+            status_code=409,
+            detail=f"The user with {user.username} is already exists in system.",
+        )
+    password_hash = gen_password_hash(user.password)
+    user = UserModel(
+        username=user.username,
+        hashedPassword=password_hash,
+        fullName=user.full_name,
+        role=user.role,
+        isActive=user.is_active,
+    )
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return user
