122a1eedac1be359f814e131fb0d983da8c9ffad
Ari Efendi <ariefendi992@gmail.com>
2023-12-11 20:57:37 +0800

    update user update

--- src/backend/app/api/endpoints_v1/user_api.py
+++ src/backend/app/api/endpoints_v1/user_api.py
@@ -1,18 +1,13 @@
-from typing import List
+from typing import Annotated, List
+from uuid import UUID
+from pydantic import Field
 from sqlalchemy import Select, select
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Body, HTTPException, Path, Query
 from app.core.dependencies import SessionDepends, gen_password_hash
 from app.models.user_model import UserModel
-from app.schemas.user_schema import UserCreateSchm, UserInDBSchm, UserOutSchm
+from app.schemas.user_schema import UserCreateSchm, UserOutSchm, UserUpdateSchm
 
 router = APIRouter()
-
-
-@router.get("/", response_model=List[UserOutSchm], summary="All Users")
-async def read_users(*, db: SessionDepends, skip: int = 0, limit: int = 100):
-    db_stmt = await db.execute(select(UserModel).offset(skip).limit(limit))
-    db_result = db_stmt.scalars()
-    return db_result
 
 
 @router.post("/", response_model=UserOutSchm, summary="Create User")
@@ -36,6 +31,62 @@ async def create_user(*, db: SessionDepends, user: UserCreateSchm):
         isActive=user.is_active,
     )
     db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+
+@router.get("/", response_model=List[UserOutSchm], summary="All Users")
+async def read_users(*, db: SessionDepends, skip: int = 0, limit: int = 100):
+    db_stmt = await db.execute(select(UserModel).offset(skip).limit(limit))
+    db_result = db_stmt.scalars()
+    return db_result
+
+
+@router.get("/{user_id}", response_model=UserOutSchm)
+async def read_user(*, db: SessionDepends, user_id: UUID = Path(...)):
+    db_stmt = await db.execute(select(UserModel).filter(UserModel.uuid == user_id))
+    user = db_stmt.scalar()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
+
+    return user
+
+
+@router.put(
+    "/{user_id}", response_model=UserOutSchm, response_model_exclude={"created_at"}
+)
+async def update_user(
+    *,
+    db: SessionDepends,
+    user_id: UUID,
+    model: UserUpdateSchm = Body(default=None),
+):
+    db_stmt = await db.execute(select(UserModel).filter_by(uuid=user_id))
+    user = db_stmt.scalar()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
+
+    if model.username is not None or model.username != "string" or model.username != "":
+        user.username = model.username
+    if model.password is not None or model.password != "string" or model.password != "":
+        pass_hash = gen_password_hash(model.password)
+        user.hashed_password = pass_hash
+    if (
+        model.full_name is not None
+        or model.full_name != "string"
+        or model.full_name != ""
+    ):
+        user.full_name = model.full_name
+    if model.role is not None or model.role != "string" or model.role != "":
+        user.role = model.role
+    if (
+        model.is_active is not None
+        or model.is_active != "string"
+        or model.is_active != ""
+    ):
+        user.is_active = model.is_active
+
     await db.commit()
     await db.refresh(user)
     return user
