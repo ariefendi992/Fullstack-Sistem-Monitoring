905a61e3ed7c7e501b9d7d781d9174f7d4b91350
Ari Efendi <ariefendi992@gmail.com>
2023-12-15 23:37:14 +0800

    change workers to reload

--- src/backend/main.py
+++ src/backend/main.py
@@ -12,6 +12,6 @@ async def main():
 
 if __name__ == "__main__":
     # asyncio.run(main())
-    config = uvicorn.Config("main:app", port=8000, log_level="info", workers=4)
+    config = uvicorn.Config("main:app", port=8000, log_level="info", reload=True)
     server = uvicorn.Server(config)
     server.run()
